b3bd0aa8228897400d7e1c8ef74288203bf57f92
Carlos Avila Aguilar <krloz1003@gmail.com>
2019-04-04 22:59:36 -0600

    Iniciando con vuetable 2

---
+++ src/components/MyVuetable.vue
@@ -0,0 +1,111 @@
+<template>
+  <div class="ui container">
+	    <div class="vuetable-pagination ui basic segment grid">
+		  	<!-- Lección 8 -->
+		  	<vuetable-pagination-info ref="paginationInfoTop"
+		  	></vuetable-pagination-info>
+		  	<!-- Lección 7 -->
+		  	<vuetable-pagination ref="paginationTop"
+		  		@vuetable-pagination:change-page="onChangePage"
+		  	></vuetable-pagination>
+		</div>  	
+		<vuetable ref="vuetable"
+		api-url="https://vuetable.ratiw.net/api/users"
+		:fields=fields
+		pagination-path=""
+		:per-page="20"
+		@vuetable:pagination-data="onPaginationData"
+		></vuetable>
+		<div class="vuetable-pagination ui basic segment grid">
+			<!-- Lección 8 -->
+			<vuetable-pagination-info ref="paginationInfo"
+			></vuetable-pagination-info>
+			<!-- Lección 7 -->
+			<vuetable-pagination ref="pagination"
+				@vuetable-pagination:change-page="onChangePage"
+			></vuetable-pagination>
+		</div>
+  </div>
+</template>
+
+<script>
+import accounting from 'accounting'
+import moment from 'moment'
+import Vuetable from 'vuetable-2/src/components/Vuetable'
+import VuetablePagination from 'vuetable-2/src/components/VuetablePagination'
+import VuetablePaginationInfo from 'vuetable-2/src/components/VuetablePaginationInfo'
+
+export default {
+  components: {
+    Vuetable,
+    VuetablePagination,
+    VuetablePaginationInfo
+  },
+  data() {
+  	return {
+  		fields: [
+  			'name', 'email',
+  			{
+  				name: 'birthdate',
+  				titleClass: 'center aligned',
+  				dataClass: 'center aligned',
+  				callback: 'formatDate|DD-MM-YYYY'
+  			}, {
+  				name: 'nickname',
+  				callback: 'allcap'
+  			}, {
+  				name: 'gender',
+  				titleClass: 'center aligned',
+  				dataClass: 'center aligned',
+  				callback: 'genderLabel'
+  			}, {
+  				name: 'salary',
+  				titleClass: 'center aligned',
+  				dataClass: 'right aligned',
+  				callback: 'formatNumber'
+  			}
+
+
+  			/* {
+  				name: 'address.line1',
+  				title: 'Address 1',
+  			}, {
+  				name: 'address.line2',
+  				title: 'Address 2',
+  			}, {
+  				name: 'address.zipcode',
+  				title: 'Zipcode'
+  			}*/
+  		]
+  	}
+  },
+  methods: {
+  	allcap (value) {
+  		return value.toUpperCase()
+  	},
+  	genderLabel (value) {
+  		return value === 'M'
+  		? '<span class="ui teal label"><i class="large man icon"></i>Male</span>'
+  		: '<span class="ui pink label"><i class="large woman icon"></i>Female</span>'
+  	},
+  	formatNumber (value) {
+  		return accounting.formatNumber(value, 2)
+  	},
+  	formatDate (value, fmt = 'D MMM YYYY') {
+  		return (value == null)
+  		? ''
+  		: moment(value, 'YYYY-MM-DD').format(fmt)
+  	},
+  	onPaginationData (paginationData) {
+		this.$refs.paginationTop.setPaginationData(paginationData)
+		this.$refs.paginationInfoTop.setPaginationData(paginationData)
+
+  		this.$refs.pagination.setPaginationData(paginationData)
+  		this.$refs.paginationInfo.setPaginationData(paginationData)
+  	},
+  	onChangePage (page) {
+  		this.$refs.vuetable.changePage(page)
+  	}
+  }
+}
+</script>
