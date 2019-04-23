0d3741e475d79b81b5d72fc3b809d3bc2f95d358
Carlos Avila Aguilar <krloz1003@gmail.com>
2019-04-22 19:51:16 -0500

    Lección 11: Usando campos especiales: sequence, handle, checkbox, component, slot

--- src/components/MyVuetable.vue
+++ src/components/MyVuetable.vue
@@ -17,7 +17,25 @@
     :multi-sort="true"
     multi-sort-key="ctrl"
 		@vuetable:pagination-data="onPaginationData"
-		></vuetable>
+		>
+    <template slot="actions" slot-scope="props">
+      <div class="custom-actions">
+        <button class="ui basic button"
+          @click="onAction('view-item', props.rowData, props.rowIndex)">
+          <i class="zoom icon"></i>
+        </button>
+        <button class="ui basic button"
+          @click="onAction('edit-item', props.rowData, props.rowIndex)">
+          <i class="edit icon"></i>
+        </button>
+        <button class="ui basic button"
+          @click="onAction('delete-item', props.rowData, props.rowIndex)">
+          <i class="delete icon"></i>
+        </button>
+      </div>
+    </template>    
+
+    </vuetable>
 		<div class="vuetable-pagination ui basic segment grid">
 			<!-- Lección 8 -->
 			<vuetable-pagination-info ref="paginationInfo"
@@ -36,6 +54,10 @@ import moment from 'moment'
 import Vuetable from 'vuetable-2/src/components/Vuetable'
 import VuetablePagination from 'vuetable-2/src/components/VuetablePagination'
 import VuetablePaginationInfo from 'vuetable-2/src/components/VuetablePaginationInfo'
+import Vue from 'vue'
+import CustomActions from './CustomActions'
+
+Vue.component('custom-actions', CustomActions)
 
 export default {
   components: {
@@ -49,7 +71,22 @@ export default {
           ascendingIcon: 'glyphicon glyphicon-chevron-up',
           descendingIcon: 'glyphicon glyphicon-chevron-down'
       },
-  		fields: [{
+  		fields: [/*{
+        name: '__sequence', // Mustra el número de secuencia de los registros en función de la información de paginación
+        title: '#',
+        titleClass: 'center aligned',
+        dataClass: 'right aligned'
+      }*/
+      /*{
+        name: '__handle', // Este fue creado específicamente porque necesitamos hacer un icono de manejador para Sortable.js
+        dataClass: 'center aligned'
+      }*/
+      /*{
+        name: '__checkbox',
+        titleClass: 'center aligned',
+        dataClass: 'center aligned'
+      }*/
+      {
   			name: 'name',
         sortField: 'name'
       }, { 
@@ -79,7 +116,13 @@ export default {
   				dataClass: 'right aligned',
   				callback: 'formatNumber',
           visible: false
-  			}
+  			}, {          
+          //name: '__component:custom-actions', // DE esta manera obtenemos las acciones desde el componente que creamos.
+          name: '__slot:actions',
+          title: 'Actions',
+          titleClass: 'center aligned',
+          dataClass: 'center aligned'
+        } 
 
 
   			/* {
@@ -121,7 +164,10 @@ export default {
   	},
   	onChangePage (page) {
   		this.$refs.vuetable.changePage(page)
-  	}
+  	},
+    onAction (action, data, index) {
+      console.log('slot) action: ' + action, data.name, index)
+    }
   }
 }
 </script>
